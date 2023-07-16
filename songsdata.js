21a4ae598d8b1973ddc622fdd4d201f3121a0ffe
KARTHIKEYA PEELA <135359651+KarthikeyaPeela@users.noreply.github.com>
2023-07-16 11:15:03 +0530

    Add files via upload

---
+++ songsdata.js
@@ -0,0 +1,28 @@
+let songs= [
+    {
+        name: 'Ammaye Sannaga',
+        path: 'songs/[iSongs.info] 02 - Ammaye Sannaga.mp3',
+        artist: 'Mani Sharma',
+        cover:'songs/Screenshot 2023-07-07 201112.png'
+
+    },
+    {
+        name: 'Peydhavi Chivarakey',
+        path: 'songs/[iSongs.info] 03 - Peydhavi Chivarakey.mp3',
+        artist: 'Anirudh Ravichander',
+        cover:'songs/155242_thumb_665.png'
+    },
+    {
+        name:'Why This Kolaveri Di',
+        path:'songs/[iSongs.info] 09 - Why This Kolaveri Di.mp3',
+        artist: 'Anirudh Ravichander',
+        cover: 'songs/3-Telugu-2012-500x500.jpg'
+    },
+    {
+        name: 'Ori Vaari',
+        path: 'songs/2-Ori Vaari.mp3',
+        artist: 'Sonthosh Narayan',
+        cover: 'songs/Dasara_1675253603.jpg'
+    },
+    
+]
